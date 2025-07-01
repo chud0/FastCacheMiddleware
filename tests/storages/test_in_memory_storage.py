991c2d403ea10ead755dd78d7529692915c762c1
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-07-01 17:07:52 +0300

    move storage fixtures to conftest

--- tests/storages/test_in_memory_storage.py
+++ tests/storages/test_in_memory_storage.py
@@ -14,28 +14,6 @@ from fast_cache_middleware.storages import InMemoryStorage
 from fast_cache_middleware.exceptions import StorageError
 
 
-@pytest.fixture
-def mock_request() -> Request:
-    return Request(scope={"type": "http", "method": "GET", "path": "/test"})
-
-
-@pytest.fixture
-def mock_response() -> Response:
-    return Response(content="test content", status_code=200)
-
-
-@pytest.fixture
-def mock_metadata() -> Metadata:
-    return {"test": "value"}
-
-
-@pytest.fixture
-def mock_store_data(
-    mock_request: Request, mock_response: Response, mock_metadata: Metadata
-) -> tp.Tuple[Response, Request, Metadata]:
-    return mock_response, mock_request, mock_metadata
-
-
 @pytest.mark.parametrize(
     "max_size, ttl, expected_error",
     [
