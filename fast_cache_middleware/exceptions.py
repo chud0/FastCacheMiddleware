8c1a863ccc68f66e55a999db9cb7f84a4e794250
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-07-08 10:37:43 +0300

    new exception - NotFoundError

--- fast_cache_middleware/exceptions.py
+++ fast_cache_middleware/exceptions.py
@@ -4,3 +4,10 @@ class FastCacheMiddlewareError(Exception):
 
 class StorageError(FastCacheMiddlewareError):
     pass
+
+
+class NotFoundError(StorageError):
+    def __init__(self, key: str, message: str = "Data not found") -> None:
+        self.key = key
+        self.message = message
+        super().__init__(f"{message}. Key: {key}.")
