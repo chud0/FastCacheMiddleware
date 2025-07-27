8b4b3269d289cddae112b852536e52e8bed941e3
Nikita Yakovlev <driftworks2013@yandex.ru>
2025-07-27 20:53:24 +0300

    add exception for ttl

--- fast_cache_middleware/exceptions.py
+++ fast_cache_middleware/exceptions.py
@@ -6,8 +6,11 @@ class StorageError(FastCacheMiddlewareError):
     pass
 
 
-class NotFoundError(StorageError):
+class NotFoundStorageError(StorageError):
     def __init__(self, key: str, message: str = "Data not found") -> None:
-        self.key = key
-        self.message = message
+        super().__init__(f"{message}. Key: {key}.")
+
+
+class TTLExpiredStorageError(StorageError):
+    def __init__(self, key: str, message: str = "TTL expired") -> None:
         super().__init__(f"{message}. Key: {key}.")
