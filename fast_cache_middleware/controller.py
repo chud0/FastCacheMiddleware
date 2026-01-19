b3f6f432b8cbecb45fea0e2d720704c7c19afc3a
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-19 12:28:30 +0300

    move generate key methods to helpers module

--- fast_cache_middleware/controller.py
+++ fast_cache_middleware/controller.py
@@ -9,6 +9,7 @@ from starlette.requests import Request
 from starlette.responses import Response
 from starlette.routing import is_async_callable
 
+from ._helpers import generate_key
 from .exceptions import FastCacheMiddlewareError
 from .schemas import CacheConfiguration
 from .storages import BaseStorage
